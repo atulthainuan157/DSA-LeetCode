ba4f85e2c8dd39f15c45dcd5b16aed96730d4c31
Atul Thainuan <atulthainuan157@gmail.com>
2025-12-06 21:17:42 +0530

    Time: 0 ms (100%), Space: 8.3 MB (12.64%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        vector<int>vec;
-        int bin=0;
-        while(n!=0) {
-            int rem=n%2;
-            vec.push_back(rem);
-            n=n/2;
-        }
-        for(int val : vec){
-            if(val==1)
+        int bin = 0;
+        while (n != 0) {
+            int rem = n % 2;
+            if(rem==1)
                 bin++;
+            n = n / 2;
         }
         return bin;
     }
