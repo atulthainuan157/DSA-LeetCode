4d1dcc7a50e28e30702f97f564638ec2e6b63f5e
Atul Thainuan <atulthainuan157@gmail.com>
2026-03-29 21:20:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (67.1%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if (s1 == s2) {
-            return true;
-        }
-        for (int i = 0; i < 2; i++) {
-            if (s1[i] == s2[i]) {
-                continue;
-            } else {
-                swap(s1[i], s1[i + 2]);
-                if (s1[i] == s2[i]) {
-                    continue;
-                } else {
-                    return false;
-                }
-            }
-        }
-        for (int i = 2; i < 4; i++) {
-            if (s1[i] != s2[i]) {
-                return false;
-            }
-        }
-        return true;
+        bool evenMatch = (s1[0] == s2[0] && s1[2] == s2[2]) ||
+                         (s1[0] == s2[2] && s1[2] == s2[0]);
+        bool oddMatch = (s1[1] == s2[1] && s1[3] == s2[3]) ||
+                        (s1[1] == s2[3] && s1[3] == s2[1]);
+        return evenMatch && oddMatch;
     }
 };
