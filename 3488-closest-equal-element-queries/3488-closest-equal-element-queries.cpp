588170a726808ebde6d6d951ee6f5b1b93057a25
Atul Thainuan <atulthainuan157@gmail.com>
2026-04-17 00:19:38 +0530

    Time: 223 ms (54.23%), Space: 217 MB (30.35%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> mp;
+
+        // store indices of each value
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        vector<int> ans;
+
+        for (int q : queries) {
+            int val = nums[q];
+            auto& v = mp[val];
+
+            if (v.size() == 1) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            int pos = lower_bound(v.begin(), v.end(), q) - v.begin();
+
+            int next = v[(pos + 1) % v.size()];
+            int prev = v[(pos - 1 + v.size()) % v.size()];
+
+            int d1 = abs(q - next);
+            int d2 = abs(q - prev);
+
+            int dist1 = min(d1, n - d1);
+            int dist2 = min(d2, n - d2);
+
+            ans.push_back(min(dist1, dist2));
+        }
+
+        return ans;
+    }
+};
