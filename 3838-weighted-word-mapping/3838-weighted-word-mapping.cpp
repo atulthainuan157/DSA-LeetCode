e0e2aaed8454a488a474b70f9fa552a306883ad0
Atul Thainuan <atulthainuan157@gmail.com>
2026-06-13 22:28:51 +0530

    Time: 0 ms (100%), Space: 43.7 MB (51.01%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string ans = "";
-        vector<char> alphabet = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r',
-                                 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i',
-                                 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
         for (int i = 0; i < words.size(); i++) {
-            int idx = 0, num = 0;
+            int num = 0;
             for (int j = 0; j < words[i].size(); j++) {
-                idx = 26 - ('z' - words[i][j]) - 1;
-                num += weights[idx];
+                num += weights[words[i][j] - 'a'];
             }
-            ans += alphabet[num % 26];
+            ans += 'z' - (num % 26);
         }
         return ans;
     }
