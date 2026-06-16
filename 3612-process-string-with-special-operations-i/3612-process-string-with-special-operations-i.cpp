698ff9bd2201c449ad7936d807d042d5b32c2890
Atul Thainuan <atulthainuan157@gmail.com>
2026-06-16 09:20:05 +0530

    Time: 2 ms (79.92%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        for (char ch : s) {
+            if (ch == '#') {
+                ans += ans;
+            } else if (ch == '*') {
+                if(!ans.empty()) {
+                    ans.pop_back();
+                }
+            } else if (ch == '%') {
+                reverse(ans.begin(), ans.end());
+            } else {
+                ans += ch;
+            }
+        }
+        return ans;
+    }
+};
