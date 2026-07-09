7faafca9ccfa2d317792442fac315f9e2f7a77e4
Atul Thainuan <atulthainuan157@gmail.com>
2026-07-10 01:53:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.16%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkGoodInteger(int n) {
+        int digitSum = 0, squareSum = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            digitSum += rem;
+            squareSum += rem * rem;
+            n /= 10;
+        }
+        return squareSum - digitSum >= 50 ? true : false;
+    }
+};
