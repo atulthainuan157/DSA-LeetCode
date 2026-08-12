84504d913738921604218e3c64950f3f7c278425
Atul Thainuan <atulthainuan157@gmail.com>
2026-08-13 01:04:33 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.05%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int count = 1;
+        for (int i = nums.size() - 1; i > 0; i--) {
+            if (nums[i] != nums[i - 1]) {
+                count++;
+                cout << nums[i] << " " << count << endl;
+            }
+            if (count == 3) {
+                return nums[i - 1];
+            }
+        }
+        return nums[nums.size() - 1];
+    }
+};
